27dc1c66b720678573f27baf5e3ac8e3925b1b10
bramlenting1234 <99068035@mydavinci.nl>
2021-10-05 12:11:10 +0200

    tafel

---
+++ tafelx .py
@@ -0,0 +1,13 @@
+# Het programma begint met de vraag: “Van welk getal wilt u de tafel zien?”
+
+# Het antwoord van de vraag wordt als argument aan een function mee gegeven die 1 parameter heeft dat een int moet zijn.
+
+# De function laat door middel van een loop de tafel (1 t/m 10) van het gekozen getal op het scherm zien.
+
+ 
+def tafel(cijver,keer):
+    return(cijver*keer)
+kiestafel=int(input("welke tafel wilt u"))
+
+for x in range (1,11):
+     print(tafel(x,kiestafel))
