dc6fdab3c6c30f24286db09f2cb2f8d61cef5044
bramlenting1234 <99068035@mydavinci.nl>
2021-10-08 13:47:23 +0200

    namen

---
+++ namen.py
@@ -0,0 +1,32 @@
+def hallonaam (namen,leeftijden):
+    return ''
+
+kiesnaam = int(input("wat is u naam "))
+kiesleeftijd = int(input("wat is u leeftijd "))
+
+
+for x in range (1,1000):
+    antwoord = hallonaam(x,kiesnaam)
+    print (hallonaam)
+    
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
--- tafelx .py
+++ tafelx .py
@@ -5,11 +5,10 @@
 # De function laat door middel van een loop de tafel (1 t/m 10) van het gekozen getal op het scherm zien.
 
  
-def tafel(a,b):
-    return(a*b)
-kiestafel=int(input("welke tafel wilt u"))
-
-for x in range(1,11):
-     antwoord = tafel(x,kiestafel)
-     print (antwoord)
+def tafel(a,b):                              # tafel = a maal b 
+    return(a*b)                              # retun functie a maal b 
+kiestafel=int(input("welke tafel wilt u"))   # kiestafel = input welke tafel wilt u 
+for x in range(1,11):                        # range in loop is 1,10 
+     antwoord = tafel(x,kiestafel)           # antwoord = tafel 10 x gekozen tafel in kies tafel 
+     print (antwoord)                        # hier print het perogramma de uitkomst van de gekozen tafels 
      
