4cd56a3625fd3f6b56a3f6f41a5d868c91fac0c8
bramlenting1234 <99068035@mydavinci.nl>
2021-10-08 10:25:11 +0200

    opdracht2

---
+++ som.py
@@ -0,0 +1,47 @@
+def addition(number1,number2):       # addition 
+    return ''                        # return waarde is plus som 
+addition = 10+12                     # som = 10 + 12   
+print(addition)                      # print uitkomst op addition 
+
+
+def subtraction(number1,number2):    # subtraction
+    return ''                        # return waarde is - som 
+subtraction = 58-34                  # som = 58 - 34
+print(subtraction)                   # print uitkomst subtraction
+
+
+def multiplication(number1,number2): # multiplication
+    return ''                        # return waarde x som
+multiplication =  6 * 7              # som 6 * 7 
+print(multiplication)                # print uitkomst multiplication
+
+
+def division(number1,number2):       # division
+    return ''                        # return waarde is : som 
+division = 144/12                    # som is 144 : 12 
+print(division)                      # print uitkomst division 
+
+
+def increment(number1,number2):      # increment 
+    return ''                        # return waarde is + som 
+increment = 12 + 1                   # som is 12 + 1 
+print(increment)                     # print uitkomst increment 
+
+def decrement(number1,number2):      # decrement 
+    return ''                        # return waarde is - som
+decrement = 34 - 1                   # som is 34 - 1
+print (decrement)                    # print uitkomst decrement 
+         
+
+
+    
+
+    
+
+
+
+
+
+
+
+
