cee8ac249b04e01cd6e1090b7c944cdb85d30c74
bramlenting1234 <99068035@mydavinci.nl>
2021-10-10 11:45:04 +0200

    aangepast

--- som.py
+++ som.py
@@ -1,37 +1,53 @@
-def addition(number1,number2):       # addition 
-    return ''                        # return waarde is plus som 
-addition = 10+12                     # som = 10 + 12   
-print(addition)                      # print uitkomst op addition 
+def addition(number1,number2):
+    result = number1+number2     
+    return result                      
+                                       
+som = addition(3,6)                   
+print(som)
 
 
-def subtraction(number1,number2):    # subtraction
-    return ''                        # return waarde is - som 
-subtraction = 58-34                  # som = 58 - 34
-print(subtraction)                   # print uitkomst subtraction
 
 
-def multiplication(number1,number2): # multiplication
-    return ''                        # return waarde x som
-multiplication =  6 * 7              # som 6 * 7 
-print(multiplication)                # print uitkomst multiplication
+def subtraction(number1,number2):
+    result = number1-number2
+    return result
+
+som = addition(20,8)  
+print (som)  
 
 
-def division(number1,number2):       # division
-    return ''                        # return waarde is : som 
-division = 144/12                    # som is 144 : 12 
-print(division)                      # print uitkomst division 
+def multiplication(number1,number2):
+    result = (number1*number2)
+    return result 
+som = multiplication(6,7)
+print (som)
 
 
-def increment(number1,number2):      # increment 
-    return ''                        # return waarde is + som 
-increment = 12 + 1                   # som is 12 + 1 
-print(increment)                     # print uitkomst increment 
+def division(number1,number2):
+    result=(number1/number2) 
+    return result 
+som = division(144,22)
+print (som)
 
-def decrement(number1,number2):      # decrement 
-    return ''                        # return waarde is - som
-decrement = 34 - 1                   # som is 34 - 1
-print (decrement)                    # print uitkomst decrement 
+
+
+def increment(number1,number2):
+    result = (number1,number2)
+    return result
+som = increment(12,1)
+print(som)    
+
+
+def decrement(number1,number2):
+    result = (number1,number2)
+    return result
+som = decrement=(34,1)
+print (som)
+    
          
+
+                   
+
 
 
     
